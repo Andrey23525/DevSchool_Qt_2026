79d65c0399a13a176035d55cf5f105bd6bb6ef97
Andrey23525 <88825623+Andrey23525@users.noreply.github.com>
2026-04-05 12:54:33 +0500

    Add task1

--- Task1/WidgetMovement/mainwindow.cpp
+++ Task1/WidgetMovement/mainwindow.cpp
@@ -1,14 +1,65 @@
-#include <QTimer>
-#include <QPushButton>
-
 #include "mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     resize(300, 600);
-/// Your code here...
 
+    _motionTimer = std::make_unique<QTimer>();
+    auto elapsed = std::make_shared<QElapsedTimer>();
+    elapsed->start();
+    _motionTimer->start(1000 / 60); // 60 FPS
+
+    connect(_motionTimer.get(), &QTimer::timeout, this, [this, elapsed]() {
+        static size_t index = 0;
+        int windowHeight = height();
+        for (auto it = _buttons.begin(); it != _buttons.end();) {
+            auto &button = *it;
+            if (!button->isVisible()) {
+                continue;
+            }
+
+            int speed = SPEED;
+            if (button->geometry().contains(mapFromGlobal(QCursor::pos()))) {
+                speed *= 2;
+            }
+            button->move(button->x(), button->y() + speed);
+
+            bool isLose = (button->y() + button->height()) >= windowHeight;
+            if (isLose) {
+                setStyleSheet(QStringLiteral("QMainWindow { background-color: red; }"));
+                setWindowTitle(QStringLiteral("Lose!"));
+                (*it)->deleteLater();
+                it = _buttons.erase(it);
+            } else {
+                ++it;
+            }
+        }
+
+        if (elapsed->elapsed() >= _creationInterval) {
+            elapsed->restart();
+            _creationInterval = MIN_CREATION_INTERVAL +
+                                QRandomGenerator64::global()->bounded(MAX_CREATION_INTERVAL - MIN_CREATION_INTERVAL);
+
+            int windowWidth = width();
+            auto button = std::make_unique<QPushButton>(QStringLiteral("*"), this);
+            button->move(QRandomGenerator64::global()->bounded(windowWidth), 100);
+            connect(button.get(), &QPushButton::clicked, this, [this, current = button.get()]() {
+                current->hide();
+                auto it = std::find_if(_buttons.begin(), _buttons.end(),
+                                       [current](const auto &element) {
+                                           return element.get() == current;
+                });
+
+                if (it != _buttons.end()) {
+                    current->deleteLater();
+                    _buttons.erase(it);
+                }
+            }, Qt::DirectConnection);
+            button->show();
+            _buttons.push_back(std::move(button));
+        }
+    }, Qt::QueuedConnection);
 }
 
 MainWindow::~MainWindow()
--- Task1/WidgetMovement/mainwindow.h
+++ Task1/WidgetMovement/mainwindow.h
@@ -2,6 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <vector>
+#include <QTimer>
+#include <QElapsedTimer>
+#include <QPushButton>
+#include <QRandomGenerator64>
 
 class QTimer;
 
@@ -14,6 +19,13 @@ public:
     ~MainWindow();
 
 private:
-    QTimer* m_timer = nullptr;
+    static constexpr double MIN_CREATION_INTERVAL = 0.1;
+    static constexpr double MAX_CREATION_INTERVAL = 1.0;
+    static constexpr int SPEED = 3;
+
+    double _creationInterval = MAX_CREATION_INTERVAL;
+    std::unique_ptr<QElapsedTimer> _elapsedTimer;
+    std::unique_ptr<QTimer> _motionTimer;
+    std::vector<std::unique_ptr<QPushButton>> _buttons;
 };
 #endif // MAINWINDOW_H
